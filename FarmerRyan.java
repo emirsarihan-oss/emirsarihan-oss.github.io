7b72fae95b9ff935e5561da7506b10884ec7d633
emirsarihan.githubio <emir.sarihan@hisarschool.k12.tr>
2026-09-20 18:39:21 +0300

    Create FarmerRyan.java

---
+++ FarmerRyan.java
@@ -0,0 +1,14 @@
+public class MyProgram
+{
+    public static void main(String[] args)
+    {
+       int b1 =10 ;
+       int b2 =20 ;
+       int b3 =10;
+       int b4 =12;
+       int b5 =10;
+       int b6 =30;
+       int beans = (b1 + b2 + b3 + b4 + b5 + b6);
+       System.out.println("Ryan has to plant " + beans + " beans.");
+    }
+}
