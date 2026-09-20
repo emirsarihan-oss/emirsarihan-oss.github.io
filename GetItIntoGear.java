97342785dbd4eecf4411ba06c9c2c9fff1d20132
emirsarihan.githubio <emir.sarihan@hisarschool.k12.tr>
2026-09-20 18:40:03 +0300

    Create GetItIntoGear.java

---
+++ GetItIntoGear.java
@@ -0,0 +1,20 @@
+public class MyProgram
+{
+    public static void main(String[] args)
+    {
+        int g1 = 12;
+        int g2 = 6;
+        int g3 = 3;
+        int g4 = 2;
+        int g5 = 6,
+        int gearamount = 5;
+        double gearratio = ((g1/g2)*(g2/g3)*(g3/g4)*(g4/g5));
+        if (gearamount/2) == 1 || 2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50
+        {
+            System.out.println(gearratio+"A");
+        
+
+        }
+    
+    }
+}
